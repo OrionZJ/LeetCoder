8fa5103bba7d51d73fab6b9815487f211dab38c3
OrionZJ <hcmcj720@163.com>
2022-02-06 12:58:43 +0800

    更新分割等和子集

--- problems/0416.分割等和子集/sulution.cpp
+++ problems/0416.分割等和子集/sulution.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int sum = sum(nums);
+        int sum = culSum(nums);
         if (sum % 2 == 0) {
-            vector<int> dp(sum / 2, 0);
+            int target = sum / 2;
+            vector<int> dp(target + 1, 0);    // 背包总容量是j，最大可以凑成j的子集总和为dp[j]
             for (int i = 0; i < nums.size(); i++) {
-                for (int j = sum / 2; j >= nums[i]; j--)
+                for (int j = target; j >= nums[i]; j--) {
+                    dp[j] = max(dp[j], dp[j - nums[i]] + nums[i]);
+                }
             }
+            if (dp[target] == target) return true;
         }
+        return false;
     }
 
-    int sum(vector<int> nums) {
+    int culSum(vector<int> nums) {
         int sum = 0;
         for (int i = 0; i < nums.size(); i++) sum += nums[i];
         return sum;

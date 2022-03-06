34e550365844f6ce712afacc5dd9de4e5fc76305
OrionZJ <hcmcj720@163.com>
2022-03-06 22:25:34 +0800

    删除字符

---
+++ problems/0583.两个字符串的删除操作/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        // dp[i][j]：以i-1为结尾的字符串word1，和以j-1位结尾的字符串word2，想要达到相等，所需要删除元素的最少次数
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1));
+        for (int i = 0; i <= word1.size(); i++) dp[i][0] = i;
+        for (int i = 0; i <= word2.size(); i++) dp[0][i] = i;
+        for (int i = 1; i <= word1.size(); i++) {
+            for (int j = 1; j <= word2.size(); j++) {
+                if (word1[i - 1] == word2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+                else dp[i][j] = min(dp[i - 1][j - 1] + 2, min(dp[i - 1][j], dp[i][j - 1]) + 1);
+            }
+        }
+        return dp.back().back();
+    }
+};

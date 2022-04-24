56891b2b4cc8b211bdc002c401e62b713323f2c9
Y9000P <hcmcj720@163.com>
2022-04-24 22:19:11 +0800

    重建队列

---
+++ problems/0406.根据身高重建队列/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    static bool cmp(const vector<int> &a,const vector<int> &b) {
+        if (a[0] == b[0]) return a[1] < b[1];
+        return a[0] > b[0];
+    }
+
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), cmp);
+        vector<vector<int>> ret;
+        for (int i = 0; i < people.size(); i++) {
+            ret.insert(ret.begin() + people[i][1], people[i]);
+        }
+        return ret;
+    }
+};
